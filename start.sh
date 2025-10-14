8482fe63840f1eedf03ea22dc71d8c7f0d2a8624
akira40-soft <netflix.accoun9858@gmail.com>
2025-10-14 19:42:34 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 if ! ollama list | grep -q mistral:7b; then
     ollama pull mistral:7b
 fi
-# Inicia o servidor Ollama
-ollama serve
+# Inicia o servidor Ollama ouvindo na porta correta
+ollama serve --host 0.0.0.0 --port ${PORT:-11434}
